f498e0cd8d1fe594aaeb744359eeca13962a3fa7
shvashakidze <shvashakidze@users.noreply.github.com>
2020-09-08 16:38:40 -0700

    Update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
-
+var port = process.env.PORT || 5000;
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -27,6 +27,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
